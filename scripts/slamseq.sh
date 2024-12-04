74cd373d0c06cf6711a8439ace68af4c9c605b68
sblab-informatics <157594438+sblab-informatics@users.noreply.github.com>
2024-12-04 17:19:08 +0000

    Update slamseq.sh

--- scripts/slamseq.sh
+++ scripts/slamseq.sh
@@ -5,9 +5,8 @@ conda activate slamseq
 ref_fasta=hg38_selected.fa
 bed_3primeUTR=gencode.v37.3primeUTR.bed
 prom_500_v37=gencode.v37.TSSminus500.bed
-output_dir=/scratchb/sblab/dhir01/Projects/Xiaoyun/SLX-21770/
+output_dir=Slamseq_out
 mkdir $output_dir
-fq_dir=/scratchb/sblab/dhir01/Projects/Xiaoyun/SLX-21770/fastq
 
 
 #=================
@@ -15,7 +14,7 @@ fq_dir=/scratchb/sblab/dhir01/Projects/Xiaoyun/SLX-21770/fastq
 for file in *gz
 do
 echo $file
-sbatch --time 36:00:00 -e %j.$file.err -J 'slam'  --mem 22G --wrap "slamdunk all -r $ref_fasta -b $bed_3primeUTR -t 5 -5 12 -n 100 -m -mv 0.2 -c 2 -rl 100 $fq_dir/$file -o $output_dir --skip-sam "
+sbatch --time 36:00:00 -e %j.$file.err -J 'slam'  --mem 22G --wrap "slamdunk all -r $ref_fasta -b $bed_3primeUTR -t 5 -5 12 -n 100 -m -mv 0.2 -c 2 -rl 100 $file -o $output_dir --skip-sam "
 done
 
 #=================
@@ -23,7 +22,7 @@ done
 # collapse counts
 
 #cd to count directory
-cd /scratchb/sblab/dhir01/Projects/Xiaoyun/SLX-21770/aligned/count
+cd Slamseq_out/aligned/count
 mkdir collapsed_count
 for file in *tsv
 do
@@ -33,7 +32,7 @@ done
 
 
 # https://t-neumann.github.io/slamdunk/docs.html#tcount-file-format -  see section Tcount file format
-cd /scratchb/sblab/dhir01/Projects/Xiaoyun/SLX-21770/aligned/count
+cd Slamseq_out/aligned/count
 for file in *_tcount.tsv
 do
 awk '!seen[$1,$2,$3]++' $file | awk '{print $4"_"$1"_"$2"_"$3"\t"$12"\t"$13"\t"$5}' > ${file%%.tsv}.reduced.tsv
